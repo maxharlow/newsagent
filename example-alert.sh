b1bba133f4555026bb848420ba824912ddb362ad
Max Harlow <maxharlow@gmail.com>
2014-10-28 15:21:29 +0000

    Take email address for example

--- example-alert.sh
+++ example-alert.sh
@@ -1,7 +1,8 @@
 read -ep 'Elasticsearch host: ' HOST
+read -ep 'Your email address: ' EMAIL
 
-curl -iX POST "http://$HOST:9200/alerts-int/alert/" -d \
-'{
+curl -iX POST "http://$HOST:9200/alerts-int/alert/" -d @- <<EOT
+{
     "name": "Unemployment rate falls under 6.2%",
     "query": {
 	"type": "ons-labour-market-statistics",
@@ -12,8 +13,9 @@ curl -iX POST "http://$HOST:9200/alerts-int/alert/" -d \
 	}
     },
     "message": "The unemployment rate fell to {{unemploymentRate}} as of {{@timestamp}}",
-    "notification": "log",
+    "notification": "email",
     "data": {
-	"recipient": "example@example.com"
+	"recipient": "$EMAIL"
     }
-}'
+}
+EOT
