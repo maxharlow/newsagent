021803f4360b5aa83f16a17fe6fe9cbd9f274eef
Max Harlow <maxharlow@gmail.com>
2014-10-30 17:09:38 +0000

    Add examples

---
+++ examples/alert-add.sh
@@ -0,0 +1,7 @@
+read -ep 'Elasticsearch host: ' HOST
+read -ep 'Source file: ' FILE
+read -ep 'Recipient email address: ' EMAIL
+
+DATA=$(sed -e "s/\$EMAIL/$EMAIL/g" $FILE)
+
+curl -iX POST "http://$HOST:9200/alerts-int/alert/" -d "$DATA"
