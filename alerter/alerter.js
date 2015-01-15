06f3ed4f0e38df6f759004710cbee8fb3ce195b3
Max Harlow <maxharlow@gmail.com>
2015-01-15 15:00:04 +0000

    Fix scrolling, split out into functions

--- alerter/alerter.js
+++ alerter/alerter.js
@@ -25,64 +25,92 @@ var send = {
 
 function run() {
     console.log('-- ' + new Date() + ' --')
-    aws.config = config.aws
-    new aws.ELB().describeLoadBalancers({ LoadBalancerNames: [ 'datastash-store' ] }, function (error, data) {
-	var elasticsearchHost = error ? 'localhost' : data.LoadBalancerDescriptions[0].DNSName
-	var elasticsearchConfig = {
-	    host: elasticsearchHost + ':' + 9200,
-	    keepAlive: false
-	}
-	console.log('\nUsing Elasticsearch host: ' + elasticsearchHost + '\n')
-	elasticsearchClient = new elasticsearch.Client(elasticsearchConfig)
+    withStore(function () {
 	elasticsearchClient.search({index: '.alerts', type: 'alert'}, function (error, response) {
 	    if (error) throw error
 	    response.hits.hits.forEach(function (hit) {
-		check(hit._source, hit._id)
+		retrieve(hit._source, hit._id)
 	    })
 	})
     })
 }
 
-function check(alert, identifier) {
-    var query = alert.query
-    query.searchType = 'scan'
-    query.scroll = '10m'
-    elasticsearchClient.search(query, function (error, queryResponse) {
+function withStore(callback) {
+    aws.config = config.aws
+    new aws.ELB().describeLoadBalancers({ LoadBalancerNames: [ 'datastash-store' ] }, function (error, data) {
+	var elasticsearchHost = error ? 'localhost' : data.LoadBalancerDescriptions[0].DNSName
+	var elasticsearchConfig = {
+	    host: elasticsearchHost + ':' + 9200,
+	    keepAlive: true
+	}
+	console.log('\nUsing Elasticsearch host: ' + elasticsearchHost + '\n')
+	elasticsearchClient = new elasticsearch.Client(elasticsearchConfig)
+	callback()
+    })
+}
+
+function retrieve(alert, identifier, matches, scrollId) {
+    matches = matches || []
+    function loop(error, queryResponse) {
 	if (error) throw error
 	var queryMatches = queryResponse.hits.hits.map(function (hit) {
 	    return hit._source
 	})
-	var shadowQuery = {
-	    searchType: 'scan',
-	    scroll: '10m',
-	    index: '.alerts',
-	    type: 'shadow',
-	    id: identifier
+	var updatedMatches = matches.concat(queryMatches)
+	if (queryResponse.hits.total !== updatedMatches.length) retrieve(alert, identifier, updatedMatches, queryResponse._scroll_id)
+	else compare(alert, identifier, updatedMatches)
+    }
+
+    if (scrollId === undefined) {
+	var query = alert.query
+	query.searchType = 'scan'
+	query.scroll = '10m'
+	elasticsearchClient.search(query, loop)
+    }
+    else {
+	var query = {
+	    scrollId: scrollId,
+	    scroll: '10m'
 	}
-	elasticsearchClient.search(shadowQuery, function (error, shadowResponse) {
-	    if (error) throw error
-	    var hasShadow = shadowResponse.hits.hits.length > 0
-	    var shadowMatches = hasShadow ? shadowResponse.hits.hits[0]._source.results : [] // entire shadow is one hit
-	    var results = queryMatches.filter(function (queryMatch) {
-		return shadowMatches.reduce(function (a, shadowMatch) {
-		    return a && JSON.stringify(queryMatch) !== JSON.stringify(shadowMatch)
-		}, true)
-	    })
-	    var document = {
-		index: '.alerts',
-		type: 'shadow',
-		id: identifier,
-		body: { results: results }
-	    }
-	    elasticsearchClient.index(document, function (error) {
-		if (error) throw error
-	    })
-	    var text = results.reduce(function (previous, result) {
-		return previous + '\n\n' + mustache.render(alert.message, result)
-	    }, '')
-	    if (hasShadow && text != '') send[alert.notification](text, alert.data)
+	elasticsearchClient.scroll(query, loop)
+    }
+}
+
+function compare(alert, identifier, queryMatches) {
+    var shadowQuery = {
+	searchType: 'scan',
+	scroll: '10m',
+	index: '.alerts',
+	type: 'shadow',
+	id: identifier
+    }
+    elasticsearchClient.search(shadowQuery, function (error, shadowResponse) {
+	if (error) throw error
+	var hasShadow = shadowResponse.hits.hits.length > 0
+	var shadowMatches = hasShadow ? shadowResponse.hits.hits[0]._source.results : [] // entire shadow is one hit
+	var results = queryMatches.filter(function (queryMatch) {
+	    return shadowMatches.reduce(function (a, shadowMatch) {
+		return a && JSON.stringify(queryMatch) !== JSON.stringify(shadowMatch)
+	    }, true)
 	})
+	check(alert, identifier, results, hasShadow)
     })
+}
+
+function check(alert, identifier, results, hasShadow) {
+    var document = {
+	index: '.alerts',
+	type: 'shadow',
+	id: identifier,
+	body: { results: results }
+    }
+    elasticsearchClient.index(document, function (error) {
+	if (error) throw error
+    })
+    var text = results.reduce(function (previous, result) {
+	return previous + '\n\n' + mustache.render(alert.message, result)
+    }, '')
+    if (hasShadow && text != '') send[alert.notification](text, alert.data)
 }
 
 run()
