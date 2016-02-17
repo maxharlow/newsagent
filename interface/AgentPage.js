431448f7b22a9a50ed94aea3bd4d6c7269139548
Max Harlow <maxharlow@gmail.com>
2016-02-17 19:09:14 +0000

    Split out recipe into separate component

--- interface/AgentPage.js
+++ interface/AgentPage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import PrettyCron from 'prettycron'
+import AgentPageRecipe from 'AgentPageRecipe.js'
 import AgentPageBuildLog from 'AgentPageBuildLog.js'
 import HTTP from 'HTTP.js'
 
@@ -22,24 +22,7 @@ export default class AgentPage extends React.Component {
             const state = this.state.state === 'starting' ? React.DOM.div({ className: 'section state creating' }, 'This agent is still being built.')
                   : this.state.state === 'failed' ? React.DOM.div({ className: 'section state failed' }, 'This agent failed to start.')
                   : undefined
-            const recipeElements = [
-                React.DOM.h3({}, 'Recipe'),
-                React.DOM.p({}, 'Runs at ' + PrettyCron.toString(this.state.recipe.schedule).toLowerCase() + '. Next run is ' + PrettyCron.getNext(this.state.recipe.schedule).toLowerCase() + '.'),
-                React.DOM.h4({}, 'Location'),
-                React.DOM.code({}, this.state.recipe.location),
-                React.DOM.span({ className: 'note' }, this.state.recipe.updatable ? 'Updates are ON.' : 'Updates are OFF.'),
-                React.DOM.h4({}, 'Setup'),
-                this.state.recipe.setup.length > 0
-                    ? React.DOM.code({}, React.DOM.ol({}, ...this.state.recipe.setup.map(step => React.DOM.li({}, step))))
-                    : React.DOM.span({ className: 'note' }, 'This agent has no setup.'),
-                React.DOM.h4({}, 'Run'),
-                React.DOM.code({}, React.DOM.ol({}, ...this.state.recipe.run.map(step => React.DOM.li({}, step)))),
-                React.DOM.h4({}, 'Result'),
-                React.DOM.code({}, this.state.recipe.result),
-                React.DOM.h4({}, 'Alerts'),
-                React.DOM.ul({ className: 'alerts' }, ...this.state.recipe.alerts.map(alert => React.DOM.li({}, alert.recipient)))
-            ]
-            const recipe = React.DOM.div({ className: 'section' }, ...recipeElements)
+            const recipe = React.createElement(AgentPageRecipe, this.state.recipe)
             const buildLog = this.state.state !== 'started' ? undefined : React.createElement(AgentPageBuildLog, { id: this.props.id })
             const buttonBarElements = [
                 React.DOM.button({ disabled: this.state.state !== 'started' }, 'Delete')
---
+++ interface/AgentPageRecipe.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import PrettyCron from 'prettycron'
+
+export default class AgentPageRecipe extends React.Component {
+
+    render() {
+        const elements = [
+            React.DOM.h3({}, 'Recipe'),
+            React.DOM.p({}, 'Runs at ' + PrettyCron.toString(this.props.schedule).toLowerCase() + '. Next run is ' + PrettyCron.getNext(this.props.schedule).toLowerCase() + '.'),
+            React.DOM.h4({}, 'Location'),
+            React.DOM.code({}, this.props.location),
+            React.DOM.span({ className: 'note' }, this.props.updatable ? 'Updates are ON.' : 'Updates are OFF.'),
+            React.DOM.h4({}, 'Setup'),
+            this.props.setup.length > 0
+                ? React.DOM.code({}, React.DOM.ol({}, ...this.props.setup.map(step => React.DOM.li({}, step))))
+            : React.DOM.span({ className: 'note' }, 'This agent has no setup.'),
+            React.DOM.h4({}, 'Run'),
+            React.DOM.code({}, React.DOM.ol({}, ...this.props.run.map(step => React.DOM.li({}, step)))),
+            React.DOM.h4({}, 'Result'),
+            React.DOM.code({}, this.props.result),
+            React.DOM.h4({}, 'Alerts'),
+            React.DOM.ul({ className: 'alerts' }, ...this.props.alerts.map(alert => React.DOM.li({}, alert.recipient)))
+        ]
+        return React.DOM.div({ className: 'section' }, ...elements)
+    }
+    
+}
