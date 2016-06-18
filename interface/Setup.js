5bca00945cbf7025a3bded6a1e08e98fcef3c08f
Max Harlow <maxharlow@gmail.com>
2016-06-18 22:44:25 +0100

    Update Interface dependencies

--- interface/Setup.js
+++ interface/Setup.js
@@ -1,9 +1,9 @@
 System.transpiler = 'babel'
 
-System.paths['page'] = '//cdnjs.cloudflare.com/ajax/libs/page.js/1.6.4/page.min.js'
-System.paths['react'] = '//cdnjs.cloudflare.com/ajax/libs/react/15.0.1/react.min.js'
-System.paths['react-dom'] = '//cdnjs.cloudflare.com/ajax/libs/react/15.0.1/react-dom.min.js'
-System.paths['moment'] = '//cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js'
+System.paths['page'] = '//cdnjs.cloudflare.com/ajax/libs/page.js/1.7.1/page.min.js'
+System.paths['react'] = '//cdnjs.cloudflare.com/ajax/libs/react/15.1.0/react.min.js'
+System.paths['react-dom'] = '//cdnjs.cloudflare.com/ajax/libs/react/15.1.0/react-dom.min.js'
+System.paths['moment'] = '//cdnjs.cloudflare.com/ajax/libs/moment.js/2.13.0/moment.min.js'
 System.paths['later'] = '//cdnjs.cloudflare.com/ajax/libs/later/1.2.0/later.min.js'
 System.paths['prettycron'] = 'https://cdn.rawgit.com/azza-bazoo/prettycron/master/prettycron.js'
 
