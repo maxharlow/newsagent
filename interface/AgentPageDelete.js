8c6f1a2b68311f40d305d1b55f6ae189458a4db2
Max Harlow <maxharlow@gmail.com>
2016-06-25 17:40:37 +0100

    Minor naming changes

--- interface/AgentPageDelete.js
+++ interface/AgentPageDelete.js
@@ -9,10 +9,10 @@ export default class AgentPageDelete extends React.Component {
     constructor() {
         super()
         this.state = { confirming: false }
-        this.deletion = this.deletion.bind(this)
+        this.send = this.send.bind(this)
     }
 
-    deletion() {
+    send() {
         HTTP.delete(Config.registry + '/agents/' + this.props.id).then(() => Page('/'))
     }
 
@@ -20,11 +20,11 @@ export default class AgentPageDelete extends React.Component {
         const dialog = !this.state.confirming ? undefined : React.createElement(Dialog, {
             text: 'Are you sure you want to delete this agent?',
             acceptText: 'Delete agent',
-            accept: this.deletion,
+            accept: this.send,
             cancel: () => this.setState({ confirming: false })
         })
-        const button = React.DOM.button({ onClick: () => this.setState({ confirming: true }) }, 'Delete')
-        return React.DOM.div({ className: 'agent-page-delete' }, button, dialog)
+        const deleteButton = React.DOM.button({ onClick: () => this.setState({ confirming: true }) }, 'Delete')
+        return React.DOM.div({ className: 'agent-page-delete' }, deleteButton, dialog)
     }
 
 }
