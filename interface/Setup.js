3c432feeb871a7521fa5ba45388427bf94e65771
Max Harlow <maxharlow@gmail.com>
2016-11-16 23:39:46 +0000

    Updates

--- interface/Setup.js
+++ interface/Setup.js
@@ -3,10 +3,10 @@ System.transpiler = 'babel'
 System.meta['*.json'] = { loader: '//cdn.rawgit.com/systemjs/plugin-json/master/json.js' }
 
 System.paths['page'] = '//cdnjs.cloudflare.com/ajax/libs/page.js/1.7.1/page.min.js'
-System.paths['react'] = '//cdnjs.cloudflare.com/ajax/libs/react/15.3.0/react.min.js'
-System.paths['react-dom'] = '//cdnjs.cloudflare.com/ajax/libs/react/15.3.0/react-dom.min.js'
-System.paths['moment'] = '//cdnjs.cloudflare.com/ajax/libs/moment.js/2.14.1/moment.min.js'
+System.paths['react'] = '//cdnjs.cloudflare.com/ajax/libs/react/15.3.2/react.js'
+System.paths['react-dom'] = '//cdnjs.cloudflare.com/ajax/libs/react/15.3.2/react-dom.js'
+System.paths['moment'] = '//cdnjs.cloudflare.com/ajax/libs/moment.js/2.16.0/moment.min.js'
 System.paths['later'] = '//cdnjs.cloudflare.com/ajax/libs/later/1.2.0/later.min.js'
-System.paths['prettycron'] = 'https://cdn.rawgit.com/azza-bazoo/prettycron/master/prettycron.js'
+System.paths['prettycron'] = '//cdn.rawgit.com/azza-bazoo/prettycron/master/prettycron.js'
 
 System.import('/Routes.js')
