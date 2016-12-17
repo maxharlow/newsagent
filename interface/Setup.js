789fe458513be54edc6e56911a75b35fcd17e5d6
Max Harlow <maxharlow@gmail.com>
2016-12-17 11:03:24 +0000

    Use HTTPS for libraries

--- interface/Setup.js
+++ interface/Setup.js
@@ -1,12 +1,12 @@
 System.transpiler = 'babel'
 
-System.meta['*.json'] = { loader: '//cdnjs.cloudflare.com/ajax/libs/systemjs-plugin-json/0.2.2/json.min.js' }
+System.meta['*.json'] = { loader: 'https://cdnjs.cloudflare.com/ajax/libs/systemjs-plugin-json/0.2.2/json.min.js' }
 
-System.paths['page'] = '//cdnjs.cloudflare.com/ajax/libs/page.js/1.7.1/page.min.js'
-System.paths['react'] = '//cdnjs.cloudflare.com/ajax/libs/react/15.4.1/react.js'
-System.paths['react-dom'] = '//cdnjs.cloudflare.com/ajax/libs/react/15.4.1/react-dom.js'
-System.paths['moment'] = '//cdnjs.cloudflare.com/ajax/libs/moment.js/2.16.0/moment.min.js'
-System.paths['later'] = '//cdnjs.cloudflare.com/ajax/libs/later/1.2.0/later.min.js'
-System.paths['prettycron'] = '//cdn.rawgit.com/azza-bazoo/prettycron/master/prettycron.js'
+System.paths['page'] = 'https://cdnjs.cloudflare.com/ajax/libs/page.js/1.7.1/page.min.js'
+System.paths['react'] = 'https://cdnjs.cloudflare.com/ajax/libs/react/15.4.1/react.js'
+System.paths['react-dom'] = 'https://cdnjs.cloudflare.com/ajax/libs/react/15.4.1/react-dom.js'
+System.paths['moment'] = 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.16.0/moment.min.js'
+System.paths['later'] = 'https://cdnjs.cloudflare.com/ajax/libs/later/1.2.0/later.min.js'
+System.paths['prettycron'] = 'https://cdn.rawgit.com/azza-bazoo/prettycron/master/prettycron.js'
 
 System.import('/Routes.js')
