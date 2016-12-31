86fdb36d52a597671b2cc375fa0ff7d2a0e377f4
Max Harlow <maxharlow@gmail.com>
2016-12-31 12:48:52 +0000

    Set max memory

--- registry/Agents.js
+++ registry/Agents.js
@@ -144,10 +144,7 @@ async function build(agent) {
         const clientInfo = await client.info()
         const context = await buildContext(client, agent.id, agent.recipe)
         const image = await buildImage(client, agent.id, context)
-        const container = await client.createContainer({
-            name: agent.id,
-            Image: image.id
-        })
+        const container = await client.createContainer({ name: agent.id, Image: image.id })
         await container.start()
         const agentStarted = {
             state: 'started',
@@ -174,8 +171,8 @@ async function buildContext(client, id, recipe) {
           + '\n' + 'COPY runner /runner'
           + '\n' + 'WORKDIR /runner'
           + '\n' + 'RUN npm install --silent'
-          + '\n' + `RUN node Start setup ${id}.json`
-          + '\n' + `CMD node Start serve ${id}.json`
+          + '\n' + `RUN node --max-old-space-size=${Config.agentMaxMemory} Start setup ${id}.json`
+          + '\n' + `CMD node --max-old-space-size=${Config.agentMaxMemory} Start serve ${id}.json`
     const tar = Promisify(TarStream.pack())
     const files = await Promisify(Glob)('../runner/*(package.json|config.json|**.js)')
     const entries = files.map(filename => {
