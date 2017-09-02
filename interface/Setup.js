fcaf6a2d3231a04a89cd5859fcaaf3c0a830acf9
Max Harlow <maxharlow@gmail.com>
2017-09-02 17:23:53 +0100

    Update to new SystemJS format

--- interface/Setup.js
+++ interface/Setup.js
@@ -1,13 +1,22 @@
-System.transpiler = 'babel'
+System.config({
+    map: {
+        'systemjs-babel-build': 'https://unpkg.com/systemjs-plugin-babel@0.0.x/systemjs-babel-browser.js',
+        'systemjs-plugin-babel': 'https://unpkg.com/systemjs-plugin-babel@0.0.x/plugin-babel.js',
+        'systemjs-plugin-json': 'https://unpkg.com/systemjs-plugin-json@0.3.x',
+        'page': 'https://unpkg.com/page@1.7.x/page.js',
+        'react': 'https://unpkg.com/react@15.6.x/dist/react.js',
+        'react-dom': 'https://unpkg.com/react-dom@15.6.x/dist/react-dom.js',
+        'moment': 'https://unpkg.com/moment@2.18.x',
+        'moment-duration-format': 'https://unpkg.com/moment-duration-format@1.3.x',
+        'later': 'https://unpkg.com/later@1.2.x/later.js',
+        'prettycron': 'https://cdn.rawgit.com/azza-bazoo/prettycron/master/prettycron.js'
+    },
+    transpiler: 'systemjs-plugin-babel',
+    meta: {
+        '*.json': {
+            loader: 'systemjs-plugin-json'
+        }
+    }
+})
 
-System.meta['*.json'] = { loader: 'https://unpkg.com/systemjs-plugin-json@0.3.x' }
-
-System.paths['page'] = 'https://unpkg.com/page@1.7.x/page.js'
-System.paths['react'] = 'https://unpkg.com/react@15.6.x/dist/react.js'
-System.paths['react-dom'] = 'https://unpkg.com/react-dom@15.6.x/dist/react-dom.js'
-System.paths['moment'] = 'https://unpkg.com/moment@2.18.x'
-System.paths['moment-duration-format'] = 'https://unpkg.com/moment-duration-format@1.3.x'
-System.paths['later'] = 'https://unpkg.com/later@1.2.x/later.js'
-System.paths['prettycron'] = 'https://cdn.rawgit.com/azza-bazoo/prettycron/master/prettycron.js'
-
-System.import('/Routes.js')
+System.import('/Routes.js').catch(console.error)
