a240f81c0834a739b943e795361bfef397018ede
Max Harlow <maxharlow@gmail.com>
2017-09-02 14:49:53 +0100

    Update interface libraries

--- interface/Setup.js
+++ interface/Setup.js
@@ -1,13 +1,13 @@
 System.transpiler = 'babel'
 
-System.meta['*.json'] = { loader: 'https://cdnjs.cloudflare.com/ajax/libs/systemjs-plugin-json/0.2.2/json.min.js' }
+System.meta['*.json'] = { loader: 'https://unpkg.com/systemjs-plugin-json@0.3.x' }
 
-System.paths['page'] = 'https://cdnjs.cloudflare.com/ajax/libs/page.js/1.7.1/page.min.js'
-System.paths['react'] = 'https://cdnjs.cloudflare.com/ajax/libs/react/15.4.1/react.js'
-System.paths['react-dom'] = 'https://cdnjs.cloudflare.com/ajax/libs/react/15.4.1/react-dom.js'
-System.paths['moment'] = 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.16.0/moment.min.js'
-System.paths['moment-duration-format'] = 'https://cdnjs.cloudflare.com/ajax/libs/moment-duration-format/1.3.0/moment-duration-format.min.js'
-System.paths['later'] = 'https://cdnjs.cloudflare.com/ajax/libs/later/1.2.0/later.min.js'
+System.paths['page'] = 'https://unpkg.com/page@1.7.x/page.js'
+System.paths['react'] = 'https://unpkg.com/react@15.6.x/dist/react.js'
+System.paths['react-dom'] = 'https://unpkg.com/react-dom@15.6.x/dist/react-dom.js'
+System.paths['moment'] = 'https://unpkg.com/moment@2.18.x'
+System.paths['moment-duration-format'] = 'https://unpkg.com/moment-duration-format@1.3.x'
+System.paths['later'] = 'https://unpkg.com/later@1.2.x/later.js'
 System.paths['prettycron'] = 'https://cdn.rawgit.com/azza-bazoo/prettycron/master/prettycron.js'
 
 System.import('/Routes.js')
